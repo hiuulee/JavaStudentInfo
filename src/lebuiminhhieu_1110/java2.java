7f7d5b28b9457f92cf27cb1c3526b894a0085038
Admin <Admin@Admin-PC>
2026-01-06 01:15:26 +0700

    Initial upload

---
+++ src/lebuiminhhieu_1110/java2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lebuiminhhieu_1110;
+
+/**
+ *
+ * @author Admin
+ */
+public class java2 {
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = x++ + x-- + --x + x++ + x++ - x;
+        System.out.println("x = " + x);
+        System.out.print("y = " + y);
+    }
+
+}
